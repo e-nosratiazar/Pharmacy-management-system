7c8710562596288d4f796261f2110958135dee45
Ensiye Nosratiazar <e.nosratiazar@gmail.com>
2022-09-18 17:57:19 +0430

    create Patient class

--- src/main/java/model/Patient.java
+++ src/main/java/model/Patient.java
@@ -1,4 +1,47 @@
 package model;
 
 public class Patient {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String nationalCode;
+
+    public Patient(String firstName, String lastName, int age, String nationalCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.nationalCode = nationalCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
 }
