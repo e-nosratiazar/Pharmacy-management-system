81c31066b612176642656173b055d821437e5391
Ensiye Nosratiazar <e.nosratiazar@gmail.com>
2022-09-19 10:29:50 +0430

    create util package

---
+++ src/main/java/util/Application.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.sql.Connection;
+
+public class Application {
+    private static Connection connection = new DbHelper().connection();
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
